f742ec2551480344d2c1fc1c21563b011b2cc8b3
Adnan Munawar <amunawar@wpi.edu>
2016-03-30 02:06:17 -0400

    Some debugging and crash avoidance

--- plot_spr.m
+++ plot_spr.m
@@ -1,9 +1,19 @@
 function plot_spr(x,xp,y,yp,xout,yout)
 global h1 h2 h3 h4 h5 h6;
-h1 = plot(x,y,'r.','markersize',18)
+h1 = plot(x,y,'r.','markersize',18);
 h2 = plot(x+xp,y+yp);
-h3 = plot([x x],[1 2],'Color','black');
+h3 = plot([x x],[y-1 y],'Color','black');
 h4 = plot(xout,yout,'r.','markersize',18);
+xtemp = zeros(numel(xout),2);
+ytemp = zeros(numel(yout),2);
+for i=1:numel(xout)
+    xtemp(i,:) = [xout(i) x];
+    ytemp(i,:) = [yout(i) y];
+end
+x_radi = reshape(xtemp.',[1,numel(xtemp)]);
+y_radi = reshape(ytemp.',[1,numel(ytemp)]);
+x_proj = reshape(xtemp,[1,numel(xtemp)]);
+y_proj = reshape(ytemp,[1,numel(ytemp)]);
 h5 = plot([xout(1) x x xout(2)],[yout(1) yout(1) yout(2) yout(2)],'Color','m');
-h6 = plot([xout(1) x xout(2)],[yout(1) y yout(2)],'Color','black');
+h6 = plot(x_radi,y_radi,'Color','black');
 end
--- spr.m
+++ spr.m
@@ -4,26 +4,29 @@
 clc;
 close all;
 clear all;
+global startx starty
 itr = 100;
-[x_mesh, y_mesh] = create_mesh_2D(itr)
-plot(x_mesh,y_mesh,'Color','green')
+startx = 4.5;
+starty = 2.5;
+[x_mesh, y_mesh] = create_mesh_2D(itr);
+plot(x_mesh,y_mesh,'Color','green');
 axis equal
 grid on;
 hold on;
 r=1;
-[x_spr, y_spr] = create_spr_2D(r)
-x = 4.5;
-y = 2;
+[x_spr, y_spr] = create_spr_2D(r);
+x = startx;
+y = starty;
 [xout, yout] =intersections(x_mesh,y_mesh,x+x_spr,y+y_spr,1);
 plot_spr(x,x_spr,y,y_spr,xout,yout);
 for j=1:5   
-x = 4.5;
-y = 2;
+x = startx;
+y = starty;
 for i=1:itr
 clear_spr_plot;
 x=x+(1/itr);
 [xout, yout] = intersections(x_mesh,y_mesh,x+x_spr,y+y_spr,1);
-plot_spr(x,x_spr,y,y_spr,xout,yout)
+plot_spr(x,x_spr,y,y_spr,xout,yout);
 pause(0.02);
 end
 disp('Backward Pass');
