1626d0eed87a408449c1d0c8739b4fcd86570d2a
Adnan Munawar <amunawar@wpi.edu>
2016-03-30 00:32:44 -0400

    First Commit

---
+++ spr.m
@@ -0,0 +1,50 @@
+clc;
+close all;
+clear all;
+itr = 100;
+body1 = linspace(2-(2/(itr/2)),0,(itr/2));
+body1 = [ones(1,(itr/2))*2,body1];
+hor = linspace(1,10,itr);
+plot(hor,body1)
+axis equal
+
+hold on;
+r=1;
+ang=0:0.01:2*pi; 
+xp=r*cos(ang);
+yp=r*sin(ang);
+x = 4.5;
+y = 2;
+[xout, yout] =intersections(hor,body1,x+xp,y+yp,1);
+h2 = plot(x+xp,y+yp);
+h3 = plot([x x],[1 2]);
+h4 = plot(xout,yout,'r.','markersize',18);
+for j=1:5   
+x = 4.5;
+y = 2;
+for i=1:itr
+delete(h2);
+delete(h3);
+delete(h4);
+x=x+(1/itr);
+[xout, yout] =intersections(hor,body1,x+xp,y+yp,1)
+h2 = plot(x+xp,y+yp);
+h3 = plot([x x],[1 2]);
+h4 = plot(xout,yout,'r.','markersize',18);
+%viscircles([x y], r,'Color','b');
+pause(0.02);
+end
+disp('Backward Pass');
+for i=itr:-1:1
+delete(h2);
+delete(h3);
+delete(h4);
+x=x-(1/itr);
+[xout, yout] =intersections(hor,body1,x+xp,y+yp,1);
+h2 = plot(x+xp,y+yp);
+h3 = plot([x x],[1 2]);
+h4 = plot(xout,yout,'r.','markersize',18);
+%viscircles([x y], r,'Color','b');
+pause(0.02);
+end
+end
