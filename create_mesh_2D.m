7c479b2dff7b52cae0f5440e209aa072d54397f4
Adnan Munawar <amunawar@wpi.edu>
2016-03-30 02:15:58 -0400

    Modification to mesh generation

--- create_mesh_2D.m
+++ create_mesh_2D.m
@@ -1,5 +1,11 @@
 function[x_mesh, y_mesh] = create_mesh_2D(itr)
-y_mesh = linspace(2-(2/(itr/2)),-3,(itr/2));
-y_mesh = [ones(1,(itr/2))*2,y_mesh];
+level = 2;
+intercept = -3;
+if level <= 0
+    y_mesh = linspace(level-(2/(itr/2)),intercept,(itr/2));
+else
+    y_mesh = linspace(level+(2/(itr/2)),intercept,(itr/2));
+end
+y_mesh = [ones(1,(itr/2))*level,y_mesh];
 x_mesh = linspace(1,10,itr);
 end
